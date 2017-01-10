9a3c2f629a1dca611af2fb857f77c0e7f9057266
Gergely Imreh <imrehg@gmail.com>
2017-01-10 17:45:39 +0000

    working on start script

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Enable ip forwarding
+echo "Enable IP forwarding"
 echo 1 > /proc/sys/net/ipv4/ip_forward
+
+echo "Starting paraprouted"
+/usr/sbin/parprouted -d eth0 wlan0
 
 # wait around
 while : ; do echo "Idling..."; sleep 600; done
